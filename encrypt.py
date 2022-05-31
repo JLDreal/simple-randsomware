1e8715630655d660139cb099cba11dbb9957f201
JLDreal <106476341+JLDreal@users.noreply.github.com>
2022-05-31 14:30:02 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -16,7 +16,6 @@ for paths, dirs, files in scandir.walk(folder):
 
 #win11 ghostfilefilter
 while i <= len(file_list)-1:
-    file_path =""
     file_path = file_list[i]
     if file_path.endswith("ini"):
         file_list.remove(file_list[i])
